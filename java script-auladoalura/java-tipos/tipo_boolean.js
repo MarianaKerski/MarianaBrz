e3f81c4a6cd197f9d294c7896d18409ce856d182
Mariana Kerski <122841053+Maribrz@users.noreply.github.com>
2024-04-10 12:04:39 -0300

    Add files via upload

---
+++ java script-auladoalura/java-tipos/tipo_boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Aula";
+const texto2 = "aula";
+
+console.log(texto1 === texto2)
---
+++ java script-auladoalura/java-tipos/tipostring.js
@@ -0,0 +1,12 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senha123!";
+
+const citacao = "Meu nome é ";
+const meuNome = "Mari";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string 
