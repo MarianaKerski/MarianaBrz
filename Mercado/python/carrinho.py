c7d14f03e16798629bf3a915fe39eedf3b97c155
Mariana Kerski <122841053+Maribrz@users.noreply.github.com>
2024-08-19 09:37:43 -0300

    Update carrinho.py

--- Mercado/python/carrinho.py
+++ Mercado/python/carrinho.py
@@ -1,29 +1,39 @@
 import os
-produtos = (
-    {'id':1, 'nome': 'abacate', 'preco': 3.99},
-    {'id':2, 'nome': 'mamao', 'preco': 7.37},
-    {'id':3, 'nome': 'pera', 'preco': 6.00},
-    {'id':4, 'nome': 'uva verde', 'preco': 14.99},
-    {'id':5, 'nome': 'pao de queijo', 'preco': 0.50},
-    {'id':6, 'nome': 'pão frances', 'preco': 1.98},
-    {'id':7, 'nome': 'bolo de fuba', 'preco': 20.60},
-    {'id':8, 'nome': 'macarrao instantaneo Nissin', 'preco': 3.98},
+UserAdmin = "Admin"
+produtos = [
+    {'id':1, 'nome': 'Abacate', 'preco': 3.99},
+    {'id':2, 'nome': 'Mamão', 'preco': 7.37},
+    {'id':3, 'nome': 'Pera', 'preco': 6.00},
+    {'id':4, 'nome': 'Uva verde', 'preco': 14.99},
+    {'id':5, 'nome': 'Pão de queijo', 'preco': 0.50},
+    {'id':6, 'nome': 'Pão frances', 'preco': 1.98},
+    {'id':7, 'nome': 'Bolo de fuba', 'preco': 20.60},
+    {'id':8, 'nome': 'Macarrao instantaneo Nissin', 'preco': 3.98},
     {'id':9, 'nome': 'Dúzia de ovos', 'preco':8.00},
     {'id':10, 'nome': 'Coca-cola original 2l', 'preco':14.99},
-    {'id':11, 'nome': 'alvejante', 'preco': 5.99},
-    {'id':12, 'nome': 'deca', 'preco': 170.00},
-    {'id':13, 'nome': 'trembolona', 'preco': 250.98}
+    {'id':11, 'nome': 'Alvejante', 'preco': 5.99},
+    {'id':12, 'nome': 'Deca', 'preco': 170.00},
+    {'id':13, 'nome': 'Trembolona', 'preco': 250.98}
     
-)
+]
 
 carrinho = []
 
 def exibirOpcoes():
+    print("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ")
     print('1 - Adicionar produto ao seu carrinho')
     print('2 - Remover prouto do seu carrinho')
     print('3 - Exibir prouto e o valor total')
     print('4 - Esvaziar Carrinho de compras')
     print('5 - Sair')
+    print("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ")
+    print ("──────────────────")
+    print("Funções Admin")
+    print ("──────────────────")
+    print("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ")
+    print('6 - Adicionar produto ao catalogo')
+    print('7 - Remover produto do catalogo')
+    print("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ")
 
 
 def exibirProdutos():
@@ -38,6 +48,31 @@ os.system
 print('┌────────────────────────────┐')
 print("│Bem vindo ao SuperAbacato 🛒│")
 print('└────────────────────────────┘')
+print('Menu de opções')
+
+def remover_itemcatalogo():
+    senha = input('Digite sua senha de Admin:')
+    if senha == UserAdmin:
+        exibirProdutos()
+        id = int(input('Digite o id do produto: '))
+        temp = []
+        for item in produtos:
+            if item['id'] != id:
+                temp.append(item)
+    if senha != UserAdmin:
+                print('Senha Errada.')
+
+def Adicionar_podutoaocatalogo():
+    senha = input('Digite sua senha de Admin:' )
+    if senha == UserAdmin:
+        exibirProdutos()
+        id = int(input('Digite o id do produto: '))
+        nome = (input('Digite o nome do produto: '))
+        preco = float(input('Digite o preco: '))
+        produtos.append({'id': id, 'nome': nome, 'preco': preco})
+        print(nome ,'Adicionado ao catalogo \n')
+    if senha != UserAdmin:
+                print('Senha Errada.')
 
 
 def obterNomeProduto(id):
@@ -49,6 +84,7 @@ def obterNomeProduto(id):
 while opcao != '5':
     exibirOpcoes()
     opcao = input('Digite a opção: ')
+    print("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ")
 
     if opcao == '1':
         exibirProdutos()
@@ -57,7 +93,9 @@ while opcao != '5':
         carrinho.append({'id': id, 'quantidade': quantidade})
 
     if opcao == '2':
-        id = int(input('Digite o id do produto que deseja remover: '))
+        exibirProdutos()
+        print (carrinho)
+        id = int(input('Digite o id do produto: '))
         temp = []
         for item in carrinho:
             if item['id'] != id:
@@ -66,7 +104,7 @@ while opcao != '5':
 
     if opcao == '3':
         arquivo=open("notafiscal.txt","a")
-        print('\n')
+        print('\n\n')
         somatorio = 0
         for item in carrinho:
             for produto in produtos:
@@ -75,32 +113,34 @@ while opcao != '5':
                     (produto['preco'] * item['quantidade'])
                     break
 
-                import datetime
-            data_atual = datetime.date.today()
-            data_formatada = data_atual.strftime("%d/%m/%Y")
+            import datetime
+        data_atual = datetime.date.today()
+        data_formatada = data_atual.strftime("%d/%m/%Y")
 
-            print(
+        print(
                 'Nome: {0} - Quantidade: {1}'.format(obterNomeProduto(item['id']), item['quantidade']))
         print('Preço total: {0}'.format(somatorio))
-        print('─────────────────')
-        continuarcompra = input("Deseja continuar comprando?(sim/nao):")
-        if continuarcompra == "não":
+        
+        continuarcompra = input("Deseja continuar comprando?(sim/não)")
+        if continuarcompra == "nao" or "não":
             if continuarcompra == "sim":
                     break
 
             nota = input("Deseja ter sua nota fiscal?(sim/não - sair)")
-            if nota == "nao" or 'não':
+            if nota == "não":
                 print('Obrigado pela preferencia! Volte logo.')
-                exit()
+                break
             if nota == "sim":
                 print("Digite seu CPF:")
             cpf = input()
-            print ("─────────────────")
+            print("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ")
+            print ("──────────────────")
             print("DATA:", (data_atual))
-            print ("─────────────────")
+            print ("──────────────────")
             print( "CPF:", cpf)
-            print ("─────────────────")
+            print ("──────────────────")
             print("PREÇO TOTAL = ",somatorio)
+            print ("──────────────────")
             arquivo.write("cpf = ")
             arquivo.write((cpf))
             arquivo.write('\n')
@@ -112,14 +152,21 @@ while opcao != '5':
             arquivo.write('--------------')
             arquivo.write('\n')
             arquivo.close()
-            print('─────────────────')
             print('Obrigado pela preferencia! Volte logo.')
             exit()
     if opcao == '4':
         carrinho = []
 
+    if opcao == '7':
+        remover_itemcatalogo()
+
+
     if opcao == '5':
         print("Volte logo!")
         exit()
+    
+    if opcao == '6':
+       Adicionar_podutoaocatalogo()
+
 
 
